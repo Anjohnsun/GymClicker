5895247332a7e21a49f6acbd178d32a9d10a7085
SobolevicDaniell <sobolevdan99@yandex.ru>
2022-06-01 19:11:55 +0300

    Add buy

--- Assets/Scripts/Main_Character.cs
+++ Assets/Scripts/Main_Character.cs
@@ -1,18 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Main_Character : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextMeshProUGUI BrutalityText;
+    public Slider sliderLV;
+
+    public int brutality;
+    public float toNextLv = 50;
+    public float realProgress = 0;
+    public int level = 0;
+    public int boost = 0;
+    public float coeffToNextLv = 1.2f;
+
+    public int firstCost = 5;
+    public int secondCost = 15;
+    
+
+
     void Start()
     {
+        brutality = 0;
+        BrutalityText = GetComponent<TextMeshProUGUI>();
+        sliderLV.value = 0;
+    }
+
+    void Update()
+    {
+        BrutalityText.text = brutality.ToString();
+
+        sliderLV.value = (realProgress / 100) * (100 / toNextLv);
+        if (realProgress >= toNextLv) { LVUp(); }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Click()
     {
-        
+        brutality += 1 + boost;
+        realProgress++;
     }
+
+    public void LVUp()
+    {
+        level++;
+        toNextLv = toNextLv * coeffToNextLv;
+        realProgress = 0;
+    }
+
+    public void BuyFirst()
+    {
+        if (brutality >= firstCost)
+        {
+            boost++;
+            brutality -= firstCost;
+        }
+    }
+    public void BuySecond()
+    {
+        if (brutality >= secondCost)
+        {
+            boost += 3;
+            brutality -= secondCost;
+        }
+    }
+
+
+    
 }
