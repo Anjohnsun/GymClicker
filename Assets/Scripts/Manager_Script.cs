b216ec2594b032819a4a10d1df8579694c6209dd
anjohnsun <anjohnsun@gmail.com>
2022-06-01 15:06:41 +0300

    Code writing started, gameManager is being filled

--- Assets/Scripts/Manager_Script.cs
+++ Assets/Scripts/Manager_Script.cs
@@ -7,34 +7,30 @@ public class Manager_Script : MonoBehaviour
 {
     private int brutality;
     private int activeBoost;
+    private int level;
 
-    [SerializeField] TMP_Text stat;
 
-    public void Click()
-    {
-        brutality = brutality + 1 + activeBoost;
-    }
 
-    public void ActiveFirstUpgrade()
-    {
-        activeBoost++;
-        brutality = brutality - 5;
-    }
-
-    public void ActiveSecondUpgrade()
-    {
-        activeBoost = activeBoost + 3;
-        brutality = brutality - 15;
-    }
-
-    void Start()
-    {
-        brutality = 0;
-        activeBoost = 0;
-    }
-
-    void Update()
-    {
-        stat.text = "Brutality - " + brutality;
-    }
 }
+
+public abstract class ImprovingState
+{
+    private int _upgradeCost;
+    private int _upgradeImprovement;
+    private int _levelRequired;
+    private int _improvementLevel;
+
+    public ImprovingState(int upgradeCost, int upgradeImprovement, int levelRequired, int improvementLevel)
+    {
+        _upgradeCost = upgradeCost;
+        _upgradeImprovement = upgradeImprovement;
+        _levelRequired = levelRequired;
+        _improvementLevel = improvementLevel;
+    }
+
+    public int UpgradeCost { get => _upgradeCost; }
+    public int UpgradeImprovement { get => _upgradeImprovement; }
+    public int LevelRequired { get => _levelRequired; }
+    public int ImprovementLevel { get => _improvementLevel; }
+}
+
