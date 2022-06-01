20ce0c879bba6ac2524dda8a860d01ada91156be
anjohnsun <anjohnsun@gmail.com>
2022-06-01 16:51:25 +0300

    ButtonPrefab changed, GManager changed, UpWindow created

--- Assets/Scripts/Manager_Script.cs
+++ Assets/Scripts/Manager_Script.cs
@@ -6,10 +6,17 @@ using TMPro;
 public class Manager_Script : MonoBehaviour
 {
     private int brutality;
-    private int activeBoost;
+    private int clickEffectiveness;
     private int level;
 
+    [SerializeField] private List<UpgradeWindow> _heroUpgrades = new List<UpgradeWindow>();
 
+    private float soundVolume;
+    private float musicVolume;
 
+    private void Start()
+    {
+        //загрузить данные из файла
+    }
 }
 
--- Assets/Scripts/UpgradeWindow.cs
+++ Assets/Scripts/UpgradeWindow.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class UpgradeWindow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int _upgradeCost;
+    [SerializeField] private int _upgradeImprovement;
+    [SerializeField] private int _levelRequired;
+    [SerializeField] private int _improvementLevel;
+
+    [SerializeField] private GameObject _blockingMask;
+    public UpgradeWindow(int upgradeCost, int upgradeImprovement, int levelRequired, int improvementLevel)
     {
-        
+        _upgradeCost = upgradeCost;
+        _upgradeImprovement = upgradeImprovement;
+        _levelRequired = levelRequired;
+        _improvementLevel = improvementLevel;
     }
 
-    // Update is called once per frame
-    void Update()
+    public int UpgradeCost { get => _upgradeCost; }
+    public int UpgradeImprovement { get => _upgradeImprovement; }
+    public int LevelRequired { get => _levelRequired; }
+    public int ImprovementLevel { get => _improvementLevel; }
+
+    public void BuyUpgrade()
     {
-        
+
     }
+
+    public void HideMask()
+    {
+        _blockingMask.SetActive(false);
+    }
+
 }
