2ad7c3d7357f51412a5eeb1f86ed0771b56a7220
anjohnsun <anjohnsun@gmail.com>
2022-06-03 15:38:28 +0300

    no message

--- Assets/Scripts/Manager_Script.cs
+++ Assets/Scripts/Manager_Script.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Events;
 
 public class Manager_Script : MonoBehaviour
 {
     public int brutality;
     public int clickEffectiveness;
-    public int level;
+    public int _level;
     public int sliderValue;
 
     [SerializeField] private List<UpgradeWindow> _heroUpgrades = new List<UpgradeWindow>();
@@ -15,9 +16,17 @@ public class Manager_Script : MonoBehaviour
     public float soundVolume;
     public float musicVolume;
 
+    public UnityEvent<int> _levelUpdater = new UnityEvent<int>();
+
     private void Start()
     {
         //загрузить данные из файла
+    }
+
+    public void LevelUpdate(int level)
+    {
+        _level = level;
+        _levelUpdater.Invoke(level);
     }
 }
 
